839404ada10a11a929370b5f08cc52584c3df3f8
Ritzz <33751927+riteshm321@users.noreply.github.com>
2018-08-16 17:23:21 +0530

    Rebased to Coolpad Patches

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 cd ../../../..
 cd system/core
-git apply -v ../../device/Infinix/X556/patches_mtk/system_core.patch
+git apply -v ../../device/coolpad/note3/patches/system_core.patch
 cd ../..
 cd bionic
-git apply -v ../device/Infinix/X556/patches_mtk/bionic.patch
+git apply -v ../device/coolpad/note3/patches/bionic.patch
 cd ..
 cd system/sepolicy
-git apply -v ../../device/Infinix/X556/patches_mtk/system_sepolicy.patch
+git apply -v ../../device/coolpad/note3/patches/system_sepolicy.patch
 cd ../..
 cd packages/apps/Settings
-git apply -v ../../../device/Infinix/X556/patches_mtk/settings.patch
+git apply -v ../../../device/coolpad/note3/patches/settings.patch
 cd ../../..
 cd frameworks/av
-git apply -v ../../device/Infinix/X556/patches_mtk/frameworks_av.patch
+git apply -v ../../device/coolpad/note3/patches/frameworks_av.patch
 cd ..
 cd native
-git apply -v ../../device/Infinix/X556/patches_mtk/frameworks_native.patch
+git apply -v ../../device/coolpad/note3/patches/frameworks_native.patch
 cd ..
 cd base
-git apply -v ../../device/Infinix/X556/patches_mtk/frameworks_base.patch
+git apply -v ../../device/coolpad/note3/patches/frameworks_base.patch
 cd ..
 cd opt/telephony
-git apply -v ../../../device/Infinix/X556/patches_mtk/frameworks_opt_telephony.patch
+git apply -v ../../../device/coolpad/note3/patches/frameworks_opt_telephony.patch
 cd ../../..
 cd packages/apps/FMRadio
-git apply -v ../../../device/Infinix/X556/patches_mtk/fmradio.patch
+git apply -v ../../../device/coolpad/note3/patches/fmradio.patch
 cd ../../..
 cd system/netd
-git apply -v ../../device/Infinix/X556/patches_mtk/system_netd.patch
+git apply -v ../../device/coolpad/note3/patches/system_netd.patch
 cd ../..
 
